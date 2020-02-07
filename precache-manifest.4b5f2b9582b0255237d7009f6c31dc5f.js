38f69808b7d904002571984592b63d338dadb6ba
scottrbtrsn <scott.peter.robertson@gmail.com>
2020-02-07 10:57:42 -0600

    Updates

---
+++ precache-manifest.4b5f2b9582b0255237d7009f6c31dc5f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b81066eb9145b1002847f3a07bff85a0",
+    "url": "/react-ui/index.html"
+  },
+  {
+    "revision": "17a2501b74225d3d9a40",
+    "url": "/react-ui/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "882ed3715fbeb4d427ab",
+    "url": "/react-ui/static/js/2.f7c43661.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-ui/static/js/2.f7c43661.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17a2501b74225d3d9a40",
+    "url": "/react-ui/static/js/main.79cb2d94.chunk.js"
+  },
+  {
+    "revision": "de76105975eb4864cd18",
+    "url": "/react-ui/static/js/runtime-main.31bff9d7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-ui/static/media/logo.5d5d9eef.svg"
+  }
+]);
